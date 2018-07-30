25808dc28eccef3de73287a4ab853f15486fcbf6
HenryITS <henryitsolutions@gmail.com>
2018-07-30 18:13:36 +0200

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC336_9dvWWMxP6EcggUPXRkPLWCkEvEO8",
+    authDomain: "clientpanelprodng.firebaseapp.com",
+    databaseURL: "https://clientpanelprodng.firebaseio.com",
+    projectId: "clientpanelprodng",
+    storageBucket: "clientpanelprodng.appspot.com",
+    messagingSenderId: "395274841998"
+  }
+};
